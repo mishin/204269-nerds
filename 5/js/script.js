245a0e66e042ab2135e0b8ab001f35726ff3723c
Keks <keks@htmlacademy.ru>
2016-09-01 18:38:45 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/script.js
+++ 5/js/script.js
@@ -1,13 +1,13 @@
- var link = document.querySelector(".btn-contacts");
+var link = document.querySelector(".btn-contacts");
 
       var popup = document.querySelector(".modal-content");
       var close = popup.querySelector(".modal-content-close");
 
       var form = popup.querySelector("form");
-      var login = popup.querySelector("[name=login]");
-      var password = popup.querySelector("[name=password]");
+      var login = popup.querySelector("[name=name]");
+      var password = popup.querySelector("[name=email]");
 
-      var storage = localStorage.getItem("login");
+      var storage = localStorage.getItem("name");
 
       link.addEventListener("click", function(event) {
         event.preventDefault();
@@ -25,14 +25,17 @@
       close.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.remove("modal-content-show");
+        popup.classList.remove("modal-error");
       });
 
       form.addEventListener("submit", function(event) {
         if (!login.value || !password.value) {
           event.preventDefault();
-          console.log("Нужно ввести логин и пароль"); 
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
         } else {
-          localStorage.setItem("login", login.value);
+          localStorage.setItem("name", login.value);
         }
       });
 
@@ -40,6 +43,7 @@
         if (event.keyCode === 27) {
           if (popup.classList.contains("modal-content-show")) {
             popup.classList.remove("modal-content-show");
+            popup.classList.remove("modal-error");
           }
         }
       });
